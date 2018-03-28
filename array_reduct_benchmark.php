aa1c069f6fba8d88820f0a3d0fb08b32e8855537
hearst-feng <jfeng@hearst.com>
2018-03-28 10:54:13 -0400

    init composer

--- array_reduct_benchmark.php
+++ array_reduct_benchmark.php
@@ -1,68 +1,10 @@
+<?php
 /*
  * https://repl.it/@HearstFeng/Array-Reduce-Benchmark
  */
-class BaseArrayHelper
-{    
-    public static function index($array, $key, $groups = [])
-    {
-        $result = [];
-        $groups = (array) $groups;
-        foreach ($array as $element) {
-            $lastArray = &$result;
-            foreach ($groups as $group) {
-                $value = static::getValue($element, $group);
-                if (!array_key_exists($value, $lastArray)) {
-                    $lastArray[$value] = [];
-                }
-                $lastArray = &$lastArray[$value];
-            }
-            if ($key === null) {
-                if (!empty($groups)) {
-                    $lastArray[] = $element;
-                }
-            } else {
-                $value = static::getValue($element, $key);
-                if ($value !== null) {
-                    if (is_float($value)) {
-                        $value = StringHelper::floatToString($value);
-                    }
-                    $lastArray[$value] = $element;
-                }
-            }
-            unset($lastArray);
-        }
-        return $result;
-    }
-    
-    public static function getValue($array, $key, $default = null)
-    {
-        if ($key instanceof \Closure) {
-            return $key($array, $default);
-        }
-        if (is_array($key)) {
-            $lastKey = array_pop($key);
-            foreach ($key as $keyPart) {
-                $array = static::getValue($array, $keyPart);
-            }
-            $key = $lastKey;
-        }
-        if (is_array($array) && (isset($array[$key]) || array_key_exists($key, $array))) {
-            return $array[$key];
-        }
-        if (($pos = strrpos($key, '.')) !== false) {
-            $array = static::getValue($array, substr($key, 0, $pos), $default);
-            $key = substr($key, $pos + 1);
-        }
-        if (is_object($array)) {
-            // this is expected to fail if the property does not exist, or __get() is not implemented
-            // it is not reliably possible to check whether a property is accessible beforehand
-            return $array->$key;
-        } elseif (is_array($array)) {
-            return (isset($array[$key]) || array_key_exists($key, $array)) ? $array[$key] : $default;
-        }
-        return $default;
-    }
-}
+require('vendor/yiisoft/yii2/helpers/BaseArrayHelper.php'); 
+
+use yii\helpers\BaseArrayHelper;
 
 $arr = [
     [
@@ -108,6 +50,6 @@ test("foreach:", function() use ($arr) {
   return $foreach;
 });
 
-test("foreach:", function() use ($arr) {
+test("yii2 ArrayHelper index:", function() use ($arr) {
   return BaseArrayHelper::index($arr, 'id');
 });
